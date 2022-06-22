7676a16e53cb747541c86029923c6ad7873e1c98
Molfar <terryprogetto@gmail.com>
2022-06-22 18:22:58 +0200

    More fonts

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const fonts = {
     "https://creazilla-store.fra1.digitaloceanspaces.com/fonts/3451956/agata-medium-font-original.ttf",
   "Keele Decorated":
     "https://creazilla-store.fra1.digitaloceanspaces.com/fonts/3466336/keele-decorated-regular-font-original.ttf",
+  Gresham:
+    "https://creazilla-store.fra1.digitaloceanspaces.com/fonts/3465128/gresham-regular-font-original.ttf",
+  Forvertz:
+    "https://creazilla-store.fra1.digitaloceanspaces.com/fonts/3452696/forvertz-regular-font-original.ttf",
+  "KayZ Handwritting":
+    "https://creazilla-store.fra1.digitaloceanspaces.com/fonts/3466330/kayz-handwritting-medium-font-original.ttf",
+  Aaram:
+    "https://creazilla-store.fra1.digitaloceanspaces.com/fonts/3451932/aaram-regular-font-original.ttf",
+  "5by7":
+    "https://creazilla-store.fra1.digitaloceanspaces.com/fonts/3451927/5by7-bold-font-original.ttf",
 };
 
 for (const [family, url] of Object.entries(fonts)) {
