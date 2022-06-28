56fb0c2f3979dc55efc1ca3c59c6c366b1d2bc37
Molfar <terryprogetto@gmail.com>
2022-06-28 12:42:01 +0200

    Show cli version

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { convert } = require("./lib/convert");
 program
   .name("fontube")
   .description("CLI to render fonts with headless browser")
-  .version("1.0.0")
+  .version(process.env.npm_package_version)
   .requiredOption("-v, --variant <name>", "Name of template file to use")
   .requiredOption("-o, --output <path>", "Output path for generated image")
   .requiredOption("-f, --family <name>", "Font family name")
